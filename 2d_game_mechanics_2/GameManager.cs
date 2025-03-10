13f99c79eea8c2d9fe29eec468af043b7c6e7afe
SA52 <sa52@Skills-Academy-52.local>
2025-03-10 18:21:38 -0400

    2D Game Mechanics Folder Push

--- 2d_game_mechanics_2/GameManager.cs
+++ 2d_game_mechanics_2/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,6 +7,8 @@ public class GameManager : MonoBehaviour
   
    public BoardManager BoardManager;
    public PlayerController PlayerController;
+   public UIDocument UIDoc;
+   private Label m_FoodLabel;
 
    public TurnManager TurnManager { get; private set;}
 
@@ -21,10 +24,14 @@ public class GameManager : MonoBehaviour
    }
     private int m_FoodAmount = 100;
     
+
     void Start()
     {
     TurnManager = new TurnManager();
     TurnManager.OnTick += OnTurnHappen;
+
+    m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+    m_FoodLabel.text = "Food : " + m_FoodAmount;
   
     BoardManager.Init();
     PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
